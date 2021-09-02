ca39697556f9737b8a26307070d1bed476e0072f
Karolin Rafalski <kr2174@caa.columbia.edu>
2021-09-02 14:33:18 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# CSS Selectors Lab
+
+In this lab, you'll have the opportunity to design a student newsletter. The content already exists -- you just need to style it to the best of your ability!
+
+---
+
+## Lab Setup
+
+### Getting started
+
+1. Fork and clone this repository.
+
+1. Navigate to the cloned repository's directory on your command line. Then, run the following command:
+
+   ```
+   npm install
+   ```
+
+   This will install the libraries needed to run the tests.
+
+1. Open up the repository in VSCode. Follow the instructions below to complete the Lab.
+
+### Tests
+
+To run the tests, you can run the following command from the command line. You will need to be in the root directory of your local directory.
+
+```
+npm test
+```
+
+This will open the Cypress testing window, where you can click to run an individual suite of tests or all of the tests at once.
+
+## Instructions
+
+To complete the tests in this lab, you will need to add the following styles. If you're unclear about any of the instructions below, remember that you can check the test file to see what exactly is being tested.
+
+- [ ] Set all paragraph text to have a font of ....
+
+Once your complete with the above tasks, your page should look something like the image below.
+
+![Completed lab image.](./assets/example-completed.png)
+
+Once you've completed these tasks, continue to style your page. Consider adding specific styles to all list items, only list items under the "Libraries" heading, the introductory paragraph, and the "Hardest Bug So Far" section.
---
+++ cypress/integration/styling_spec.js
@@ -0,0 +1,93 @@
+describe("Styling", () => {
+  describe("Text", () => {
+    it("all paragraphs have their font set to 'Helvetica, sans-serif'", () => {
+      cy.get("p").each((paragraph) => {
+        cy.wrap(paragraph).should(
+          "have.css",
+          "font-family",
+          "Helvetica, sans-serif"
+        );
+      });
+    });
+
+    it("all headings have their font set to 'Gerogia, serif'", () => {
+      cy.get("h1, h2, h3, h4, h5, h6").each((paragraph) => {
+        cy.wrap(paragraph).should("have.css", "font-family", "Georgia, serif");
+      });
+    });
+
+    it("all text inside the `body` element is centered", () => {
+      cy.get("body").should("have.css", "text-align", "center");
+    });
+  });
+
+  describe("Images", () => {
+    it("should have an image with the class 'profile-photo'", () => {
+      cy.get("img.profile-photo").should("exist");
+    });
+
+    it("should style all images with the 'profile-photo' class so they have a border that is `1px` wide, solid, and the color `blue`", () => {
+      cy.get("img.profile-photo").should(
+        "have.css",
+        "border",
+        "1px solid rgb(0, 0, 255)"
+      );
+    });
+  });
+
+  describe("Headers", () => {
+    it("should style all headers as being bold and having a blue underline", () => {
+      cy.get("h1, h2, h3, h4, h5, h6").each((heading) => {
+        cy.wrap(heading)
+          .should("have.css", "font-weight", "700")
+          .should(
+            "have.css",
+            "text-decoration",
+            "underline solid rgb(0, 0, 255)"
+          );
+      });
+    });
+
+    it("should have an `h2` with the class 'skills-header'", () => {
+      cy.get("h2.skills-header").should("exist");
+    });
+
+    it("should style the 'skills-header' class so that the text is 'rebeccapurple'", () => {
+      cy.get("h2.skills-header")
+        .should("exist")
+        .contains("Here are some of my Skills!")
+        .should("have.css", "color", "rgb(102, 51, 153)");
+    });
+  });
+
+  describe("Links", () => {
+    it("should have links be the color `red`", () => {
+      cy.get("a").each((link) => {
+        cy.wrap(link).should("have.css", "color", "rgb(255, 0, 0)");
+      });
+    });
+  });
+
+  describe("Language list items", () => {
+    it("should have a `ul` with the class 'languages-list'", () => {
+      cy.get("ul.languages-list").should("exist");
+    });
+
+    it("should set all list items within 'languages-list' to have a globe image as a bullet point", () => {
+      cy.get(".languages-list")
+        .invoke("css", "list-style-image")
+        .should("contain", "globe.png");
+    });
+
+    it("should not change any other list items", () => {
+      cy.get(".languages-list")
+        .invoke("css", "list-style-image")
+        .should("contain", "globe.png");
+
+      cy.get("ul")
+        .last()
+        .invoke("css", "list-style-image")
+        .should("not.contain", "globe.png");
+    });
+  });
+});
